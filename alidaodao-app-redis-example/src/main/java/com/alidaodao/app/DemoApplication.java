1896a1c44fd373c2ab0eb66e327757524162acb5
jack <ssliunian@yeah.net>
2021-05-29 23:15:04 +0800

    add redis spring boot starter

---
+++ alidaodao-app-redis-example/src/main/java/com/alidaodao/app/DemoApplication.java
@@ -0,0 +1,27 @@
+package com.alidaodao.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    @Autowired
+    private RedisClient redisClient;
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+
+    @GetMapping(value = "/dev")
+    public String addRedis(@RequestParam(value = "test") String test) {
+        redisClient.get(3).set("1", test);
+        return redisClient.get(3).get("1");
+    }
+}
---
+++ spring-boot-redis-starter/src/main/java/com/alidaodao/app/RedisServiceAutoConfiguration.java
@@ -0,0 +1,53 @@
+package com.alidaodao.app;
+
+import com.alidaodao.app.config.RedisConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableConfigurationProperties(RedisProperties.class)
+@ConditionalOnClass(RedisClient.class)
+@ConditionalOnProperty(prefix="spring.redis",value="enabled",matchIfMissing=true)
+public class RedisServiceAutoConfiguration {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RedisServiceAutoConfiguration.class);
+
+    @Autowired
+    private RedisProperties redisProperties;
+
+    @Bean(value = "redisClient")
+    @ConditionalOnMissingBean(RedisClient.class)
+    public RedisClient redisClient() {
+        if (!redisProperties.isEnable()) {
+            LOGGER.warn("[REDIS-CLIENT] redis enable is false");
+            return null;
+        }
+        if (Objects.isNull(redisProperties.getIndexes()) || redisProperties.getIndexes().length <= 0) {
+            LOGGER.warn("[REDIS-CLIENT] redis indexes is empty");
+            return null;
+        }
+        RedisConfig redisConfig = new RedisConfig();
+        redisConfig.setHost(redisProperties.getHost());
+        redisConfig.setPort(redisProperties.getPort());
+        redisConfig.setPassword(redisProperties.getPassword());
+        //此处为需要创建的redis db实例
+        Set<Integer> redisDbIndex = Arrays.stream(redisProperties.getIndexes()).collect(Collectors.toSet());
+        RedisClient redisClient = new RedisClient(redisConfig, redisDbIndex);
+        redisClient.setDefaultIndex(redisProperties.getDefaultIndex());
+        LOGGER.info("[REDIS-CLIENT]redis client init success,you can use indexes: {}", redisDbIndex);
+        return redisClient;
+    }
+}
