e919873ee702c5383b60ffdc628fb769174e970c
bosong <songbo2021@outlook.com>
2021-12-20 11:23:30 +0800

    feat(reids): add tests

--- src/main/java/com/alidaodao/app/commons/Expire.java
+++ src/main/java/com/alidaodao/app/commons/Expire.java
@@ -16,39 +16,39 @@ public class Expire {
     /**
      * 3分钟
      */
-    public static Expire m3 = new Expire(60 * 3);
+    public static Expire m3 = new Expire(180);
     /**
      * 5分钟
      */
-    public static Expire m5 = new Expire(60 * 5);
+    public static Expire m5 = new Expire(300);
     /**
      * 10分钟
      */
-    public static Expire m10 = new Expire(60 * 10);
+    public static Expire m10 = new Expire(600);
     /**
      * 30分钟
      */
-    public static Expire m30 = new Expire(60 * 30);
+    public static Expire m30 = new Expire(1800);
     /**
      * 31分钟
      */
-    public static Expire m31 = new Expire(60 * 31);
+    public static Expire m31 = new Expire(1860);
     /**
      * 1小时
      */
-    public static Expire h1 = new Expire(60 * 60);
+    public static Expire h1 = new Expire(3600);
     /**
      * 2 小时
      */
-    public static Expire h2 = new Expire(2 * 60 * 60);
+    public static Expire h2 = new Expire(7200);
     /**
      * 6 小时
      */
-    public static Expire h6 = new Expire(6 * 60 * 60);
+    public static Expire h6 = new Expire(1960);
     /**
      * 2小时1分钟
      */
-    public static Expire h2PlusM1 = new Expire(121 * 60);
+    public static Expire h2PlusM1 = new Expire(1860);
     /**
      * 1天
      */
@@ -69,11 +69,11 @@ public class Expire {
     private final long time;
     private final EXPX expx;
 
-    private Expire(long time) {
+    public Expire(long time) {
         this(time, EXPX.SECONDS);
     }
 
-    private Expire(long time, EXPX expx) {
+    public Expire(long time, EXPX expx) {
         this.time = time;
         this.expx = expx;
     }
--- src/test/java/com.alidaodao.com/RedisClientTest.java
+++ src/test/java/com.alidaodao.com/RedisClientTest.java
@@ -3,7 +3,9 @@ package com.alidaodao.com;
 import com.alidaodao.app.RedisClient;
 import com.alidaodao.app.commons.Expire;
 import com.alidaodao.app.config.RedisConfig;
+import com.alidaodao.app.service.RedisService;
 
+import java.nio.charset.StandardCharsets;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,18 +16,20 @@ public class RedisClientTest {
 
 
     public static void main(String[] args) {
-        RedisClient redisClient = getRedisClient();
-        for (int i = 0; i < 100; i++) {
-            String test = redisClient.getByDefault().set("test:" + i, "test", Expire.h2);
-            System.out.println(test+" " + i);
-        }
+        RedisService cache = getRedisClient().getByDefault();
+        //单元测试
+        cache.set("1234".getBytes(StandardCharsets.UTF_8),"abcd".getBytes(StandardCharsets.UTF_8));
+        cache.set("5678","dadhd");
+        cache.set("91011","djfhd",new Expire(10));
+        cache.set("111213".getBytes(StandardCharsets.UTF_8),"dfff".getBytes(StandardCharsets.UTF_8),new Expire(100));
+        cache.setex("111213",100,"12999");
     }
 
     private static RedisClient getRedisClient(){
         RedisConfig redisConfig = new RedisConfig();
         redisConfig.setRedisHost("127.0.0.1");
         redisConfig.setRedisPort(6379);
-        redisConfig.setRedisPwd("test");
+        redisConfig.setRedisPwd("");
         //此处为需要创建的redis db实例
         Set<Integer> redisIndexes = new HashSet<>();
         redisIndexes.add(0);
